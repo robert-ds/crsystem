0ec371923b051dd8d69c312bdb24370da2efb545
Robert Vásquez <vasquezrober09@gmail.com>
2023-02-28 16:01:43 -0400

    Basic structure for project and package definition

---
+++ src/system/Main.java
@@ -0,0 +1,15 @@
+package system;
+
+/**
+ * Motto: Imagine - Model - Program
+ * Created with IntelliJ IDEA
+ * Created By Robert Vásquez
+ * Date: 28/2/23
+ * Time: 3:54 p. m.
+ */
+
+public class Main {
+  public static void main(String[] args){
+
+  }
+}
---
+++ src/system/employeeGUI/frmEmployees.java
@@ -0,0 +1,11 @@
+package system.employeeGUI;
+
+/**
+ * Motto: Imagine - Model - Program
+ * Created with IntelliJ IDEA
+ * Created By Robert Vásquez
+ * Date: 28/2/23
+ * Time: 3:59 p. m.
+ */
+public class frmEmployees {
+}
