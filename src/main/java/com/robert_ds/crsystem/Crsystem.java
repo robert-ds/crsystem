59283105458d1026fc03c426533f36d8f14625ed
Robert Vásquez <vasquezrober09@gmail.com>
2023-09-21 15:40:40 -0400

    finished GUI

---
+++ src/main/java/com/robert_ds/crsystem/Crsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.robert_ds.crsystem;
+
+/**
+ *
+ * @author robert
+ */
+public class Crsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/robert_ds/crsystem/employeeBL/employeesBL.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.robert_ds.crsystem.employeeBL;
+
+/**
+ *
+ * @author robert
+ */
+public class employeesBL {
+    
+}
